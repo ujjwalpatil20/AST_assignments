96efffc5e1491a3769315282bcecb8e89a215c27
Kamran Khowaja <kamrankhowaja@yahoo.com>
2024-06-26 13:34:39 +0200

    Moved Coppeliasim Code (Raw Code)

---
+++ coppeliasim_integration/coppeliasim_integration/simplepublisher.py
@@ -0,0 +1,26 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class HelloWorldPublisher(Node):
+    def __init__(self):
+        super().__init__('hello_world_publisher')
+        self.publisher_ = self.create_publisher(String, 'hello_world_topic', 10)
+        timer_period = 1.0  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = 'Hello, World!'
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = HelloWorldPublisher()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
